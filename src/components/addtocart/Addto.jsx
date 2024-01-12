7aac2d6d413b402564fb7324bea864e3bdc01b52
arihant-jain0 <jainari1208@gmail.com>
2024-01-12 18:36:57 +0530

    something is coming

--- src/components/addtocart/Addto.jsx
+++ src/components/addtocart/Addto.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Addto() {
   return (
     <>
@@ -28,18 +28,15 @@ function Addto() {
                 </span>
               </div>
             </div>
-            <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+            {/* products */}
+            <div className="flex items-center hover:bg-gray-100 -ml-[5rem] mr-0 py-5">
               <div className="flex w-2/5">
                 <div className="w-20">
-                  <img
-                    className="h-24"
-                    src="https://drive.google.com/file/d/18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z/view?usp=sharing"
-                    alt=""
-                  />
+                 
                 </div>
                 <div className="flex flex-col justify-between ml-4 flex-grow">
-                  <span className="font-bold text-sm">Iphone 6S</span>
-                  <span className="text-red-500 text-xs">Apple</span>
+                  <span className="font-bold text-sm"></span>
+                  <span className="text-red-500 text-xs"></span>
                   <a
                     href="#"
                     className="font-semibold hover:text-red-500 text-gray-500 text-xs"
@@ -70,25 +67,21 @@ function Addto() {
                 </svg>
               </div>
               <span className="text-center w-1/5 font-semibold text-sm">
-                $400.00
+                
               </span>
               <span className="text-center w-1/5 font-semibold text-sm">
-                $400.00
+                
               </span>
             </div>
 
-            <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+            <div className="flex items-center hover:bg-gray-100 -ml-[5rem] mr-0 py-5">
               <div className="flex w-2/5">
                 <div className="w-20">
-                  <img
-                    className="h-24"
-                    src="https://drive.google.com/file/d/18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z/view?usp=sharing"
-                    alt=""
-                  />
+                 
                 </div>
                 <div className="flex flex-col justify-between ml-4 flex-grow">
-                  <span className="font-bold text-sm">Xiaomi Mi 20000mAh</span>
-                  <span className="text-red-500 text-xs">Xiaomi</span>
+                  <span className="font-bold text-sm"></span>
+                  <span className="text-red-500 text-xs"></span>
                   <a
                     href="#"
                     className="font-semibold hover:text-red-500 text-gray-500 text-xs"
@@ -119,25 +112,21 @@ function Addto() {
                 </svg>
               </div>
               <span className="text-center w-1/5 font-semibold text-sm">
-                $40.00
+                
               </span>
               <span className="text-center w-1/5 font-semibold text-sm">
-                $40.00
+                
               </span>
             </div>
 
-            <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+            <div className="flex items-center hover:bg-gray-100 -ml-[5rem] mr-0 py-5">
               <div className="flex w-2/5">
                 <div className="w-20">
-                  <img
-                    className="h-24"
-                    src="https://drive.google.com/file/d/18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z/view?usp=sharing"
-                    alt=""
-                  />
+                  
                 </div>
                 <div className="flex flex-col justify-between ml-4 flex-grow">
-                  <span className="font-bold text-sm">Airpods</span>
-                  <span className="text-red-500 text-xs">Apple</span>
+                  <span className="font-bold text-sm"></span>
+                  <span className="text-red-500 text-xs"></span>
                   <a
                     href="#"
                     className="font-semibold hover:text-red-500 text-gray-500 text-xs"
@@ -167,12 +156,13 @@ function Addto() {
                 </svg>
               </div>
               <span className="text-center w-1/5 font-semibold text-sm">
-                $150.00
+                
               </span>
               <span className="text-center w-1/5 font-semibold text-sm">
-                $150.00
+                
               </span>
             </div>
+
 
             <a
               href="#"
@@ -184,7 +174,9 @@ function Addto() {
               >
                 <path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" />
               </svg>
+              <Link to="/">
               Continue Shopping
+              </Link>
             </a>
           </div>
 
--- src/components/homecart/Homecart.jsx
+++ src/components/homecart/Homecart.jsx
@@ -31,50 +31,46 @@ const Homecart = ({ Men }) => {
     // Focus the new window
     newWindow.focus();
   };
-  const [cartItems, setCartItems] = useState([]);
-
-  const addToCart = (product) => {
-    setCartItems([...cartItems, product]);
-  };
+  
 
   return (
     <>
-      <div className="cursor-pointer flex-col flex items-center w-[15rem] mx-5 bg-white rounded-lg shadow-lg overflow-hidden border">
-        <div className="h-[13rem] w-[10rem]" onClick={handleView}>
-          {/* Ensure that Men is defined before accessing its properties */}
-          {Men && Men.image && (
-            <img
-              className="object-cover rounded-sm object-top w-full h-[14rem]"
-              src={Men.image}
-              alt=""
-            />
-          )}
-        </div>
+  <div className="cursor-pointer flex-col flex items-center w-[15rem] mx-5 bg-white rounded-lg shadow-lg overflow-hidden border">
+    <div className="h-[13rem] w-[10rem]" onClick={handleView}>
+      {/* Ensure that Men is defined before accessing its properties */}
+      {Men && Men.image && (
+        <img
+          className="object-cover rounded-sm object-top w-full h-full"
+          src={Men.image}
+          alt=""
+        />
+      )}
+    </div>
 
-        <div className="p-4">
-          {/* Check if Men is defined before accessing its properties */}
-          {Men && (
-            <>
-              <h3 className="text-lg font-medium text-gray-900 ">{Men.brand} </h3>
-              <p className="mt-2 text-sm font-normal text-gray-500 ">{Men.title}</p>
-              <h3 className="font-semibold text-lg">
-                ₹{Men.discountedPrice}
-                <span className="text-gray-500 ml-4 line-through ">
-                  ₹{Men.price}
-                </span>
-              </h3>
-            </>
-          )}
-        </div>
+    <div className="p-5">
+      {/* Check if Men is defined before accessing its properties */}
+      {Men && (
+        <>
+          <h3 className="text-lg font-medium text-gray-900 ">{Men.brand} </h3>
+          <p className="mt-2 text-sm font-normal text-gray-500 ">{Men.title}</p>
+          <h3 className="font-semibold text-lg">
+            ₹{Men.discountedPrice}
+            <span className="text-gray-500 ml-4 line-through ">
+              ₹{Men.price}
+            </span>
+          </h3>
+        </>
+      )}
+    </div>
 
-        <button
-          onClick={() => addToCart({ name: Men?.title, price: Men?.discountedPrice, image:Men?.image })}
-          className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 mt-4"
-        >
-          Add to Cart
-        </button>
-      </div>
-    </>
+    <button
+      className="bg-indigo-700 rounded-md font-semibold font-sans mb-4 hover:bg-indigo-500 shadow-md text-white px-6 py-2 mt-1"
+    >
+      Add to Cart
+    </button>
+  </div>
+</>
+
   );
 };
 
